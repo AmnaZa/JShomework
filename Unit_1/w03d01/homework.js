8d24c4e7545c9477c51a2161e0ed79518b05bb91
AmnaZa <113046745+AmnaZa@users.noreply.github.com>
2023-06-07 20:51:46 -0400

    Console.log Hamster and Person

--- Unit_1/w03d01/homework.js
+++ Unit_1/w03d01/homework.js
@@ -16,45 +16,75 @@ class Hamster {
     }
 }
 class Person {
-    constructor (name) {
-        this.age = 0;
-        this.height = 0;
-        this.weight = 0;
-        this.mood = 0;
-        this.hamsters = [];
-        this.bankAccount = 0;
+    constructor(name){
+      this.name = name
+      this.age = 0
+      this.height = 0
+      this.weight = 0
+      this.mood = 0
+      this.hamsters = []
+      this.bankAccount = 0
     }
-    getName () {
-        return this.name;
+    getName(){
+      return this.name
     }
-    getAge() {
-        return this.age;
+    getAge(){
+      return this.age
     }
-
-    getWeight() {
-        return this.weight;
+    getWeight(){
+      return this.weight
     }
-    greet () {
-        console.log('Hello, my name is ${this.name}!');
-
+    greet(){
+      console.log(`I am ${this.name} hello`)
+    }
+    eat(){
+      this.weight++
+      this.mood++
     }
     exercise(){
-        this.weight -= 1;
+      this.weight--
     }
-    agedUp(){
-        this.age += 1;
-        this.height += 1;
-        this.weight += 1;
-        this.mood -= 1;
-        this.bankAccount += 10;
+    ageUp(){
+      this.age++
+      this.height++
+      this.weight++
+      this.mood--
+      this.bankAccount+=10
     }
-    buyHamster (){
-        this.hamster.push(hamster);
-        this.mood += 10;
-        this.bankAccount -= hamster.getPrice();
+    buyHamster(hamster){
+      this.hamsters.push(hamster)
+      this.mood+=10
+      this.bankAccount-=hamster.getPrice()
+    }
   }
 
+const timmy = new Person('Timmy')
+for(let i =0; i < 5; i++){
+  timmy.ageUp()
 }
+for(let i =0; i < 5; i++){
+  timmy.eat()
+}
+for(let i =0; i < 5; i++){
+  timmy.exercise()
+}
+for(let i =0; i < 9; i++){
+  timmy.ageUp()
+}
+const gus = new Hamster('Gus')
+gus.owner = 'Timmy'
+timmy.buyHamster(gus)
 
+for(let i =0; i < 15; i++){
+  timmy.ageUp()
+}
+timmy.eat()
+timmy.eat()
+timmy.exercise()
+timmy.exercise()
 
+console.log(`Timmy's age: ${timmy.age}`);
+console.log(`Timmy's weight: ${timmy.weight}`);
+console.log(`Timmy's mood: ${timmy.mood}`);
 
+console.log(`Gus's owner: ${gus.owner}`);
